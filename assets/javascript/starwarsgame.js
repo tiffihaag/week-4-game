27e05e4fbc866db497971c8607acbb324dd81b4f
tiffihaag <tiffihaag@gmail.com>
2016-07-18 08:25:28 -0400

    Stuck on the Leia button

--- assets/javascript/starwarsgame.js
+++ assets/javascript/starwarsgame.js
@@ -1,89 +1,67 @@
-var 
-
-
-
-
-
-
-
-		// Javascript function that wraps everything
-		$(document).ready(function(){
-			// Gets Link for Theme Song
-	        var audioElement = document.createElement('audio');
-	        audioElement.setAttribute('src', 'Assets/captainplanet24.mp3');
-			// Theme Button
-			$(".themeButton").on("click", function(){
-        		audioElement.play();
-
-		var laughElement = document.createElement('audio');
-	        laughElement.setAttribute('src', 'Assets/captainplanet24.mp3');
-			// Theme Button
-			$(".laughButton").on("click", function(){
-        		laughElement.play();
-
-
-			});
-			$(".pauseButton").on("click", function(){
-        		audioElement.pause();
-			});
-			// Size Buttons
-			$(".normalButton").on("click", function(){
-	    		$(".captainplanet").animate({height: '300px'});
-			}); 
-			$(".growButton").on("click", function(){
-	    		$(".captainplanet").animate({height: '700px', width: '700px'});
-
-			}); 
-			$(".shrinkButton").on("click", function(){
-	    		$(".captainplanet").animate({height: '100px'});
-			}); 
-			// Visibility Buttons
-			$(".visButton").on("click", function(){
-	    		$(".captainplanet").animate({opacity: '1'});
-			});
-			$(".invisButton").on("click", function(){
-	    		$(".captainplanet").animate({opacity: '0.05'});
-			});
-			// Move Buttons
-			$(".upButton").on("click", function(){
-				$(".captainplanet").animate({top:"-=200px"}, "normal");
-			});
-			$(".downButton").on("click", function(){
-				$(".captainplanet").animate({top:"+=200px"}, "normal");
-			});
-			$(".leftButton").on("click", function(){
-				$(".captainplanet").animate({left:"-=200px"}, "normal");
-			});
-			$(".rightButton").on("click", function(){
-				$(".captainplanet").animate({left:"+=200px"}, "normal");
-			});
-			$(".backButton").on("click", function(){
-				$(".captainplanet").animate({top:"50px", left: "80px"}, "fast");
-			});
-			// Move Buttons (Keyboard Down)
-			$(document).keyup(function(e){
-				switch (e.which){
-					case 40:
-    			$(".captainplanet").animate({top:"+=200px"}, "normal");}
-			});
-			// Move Buttons (Keyboard Right)
-			$(document).keyup(function(e){
-				switch (e.which){
-					case 39:
-    			$(".captainplanet").animate({left:"+=200px"}, "normal");}
-			});
-			// Move Buttons (Keyboard Up)
-			$(document).keyup(function(e){
-				switch (e.which){
-					case 38:
-    			$(".captainplanet").animate({top:"-=200px"}, "normal");}
-			});
-			// Move Buttons (Keyboard Left)
-			$(document).keyup(function(e){
-				switch (e.which){
-					case 37:
-    			$(".captainplanet").animate({left:"-=200px"}, "normal");}
-			});
-			
-		});
+//The Theme Song
+$(document).ready(function(){
+	$(".themeButton1").hide();
 	
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'assets/confrontation.mp3');
+	// Theme Button
+	$(".themeButton").on("click", function(){
+		audioElement.play();
+		$(".themeButton").hide();
+		$(".themeButton1").show();
+	});
+	
+
+	$(".themeButton1").on("click", function(){
+		audioElement.pause();
+		$(".themeButton1").hide();
+		$(".themeButton").show();
+	});
+});
+	
+//The Variables
+	var healthPoints = {
+		leia: 120,
+		ahsoka: 180,
+		padme: 100,
+		asajj: 150
+	}
+
+	var attackPower = [];
+
+	var counterAttackPower = {
+		leia: 8,
+		ahsoka: 25,
+		padme: 5,
+		asajj: 15
+	}
+	
+//The Buttons
+$(".btnLeia").on("click", function(){
+	console.log ("I clicked Leia");
+	$(".imgAhsoka").appendTo("#row2");
+	$(".imgPadme").appendTo("#row2");
+	$(".imgAsajj").appendTo("#row2");
+  });
+
+$(".btnAhsoka").on("click", function(){
+	console.log ("I clicked Ahsoka");
+	$(".imgLeia").appendTo("#row2");
+	$(".imgPadme").appendTo("#row2");
+	$(".imgAsajj").appendTo("#row2");
+  });
+
+$(".btnPadme").on("click", function(){
+	console.log ("I clicked Padme");
+	$(".imgAhsoka").appendTo("#row2");
+	$(".imgLeia").appendTo("#row2");
+	$(".imgAsajj").appendTo("#row2");
+  });
+
+$(".btnAsajj").on("click", function(){
+	console.log ("I clicked Asajj");
+	$(".imgAhsoka").appendTo("#row2");
+	$(".imgPadme").appendTo("#row2");
+	$(".imgLeia").appendTo("#row2");
+  });
+
